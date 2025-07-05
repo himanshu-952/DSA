fe06142959e57644f67a58cd64a38effa3627132
himanshu-952 <himanshukhatri952@gmail.com>
2025-07-05 23:25:08 +0530

    ✨ Added LeetCode Array problems: Reverse, Kadane, Trapping Rain Water, Stock Buy/Sell

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,17 @@
+public class ReverseArray {
+    class Solution {
+    public void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+
+        while(left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
+
+}
