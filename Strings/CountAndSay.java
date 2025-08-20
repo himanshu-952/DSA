041f6ca6a3016bf936ed2b7b29091c5bcffdec6e
himanshu-952 <himanshukhatri952@gmail.com>
2025-08-20 23:02:57 +0530

    added

---
+++ Strings/CountAndSay.java
@@ -0,0 +1,22 @@
+package Strings;
+
+public class CountAndSay {
+    class Solution {
+    public String countAndSay(int n) {
+    if (n == 1) return "1";
+    String prev = countAndSay(n - 1);
+    StringBuilder res = new StringBuilder();
+    int count = 1;
+    for (int i = 1; i <= prev.length(); i++) {
+        if (i < prev.length() && prev.charAt(i) == prev.charAt(i - 1)) {
+            count++;
+        } else {
+            res.append(count).append(prev.charAt(i - 1));
+            count = 1;
+        }
+    }
+    return res.toString();
+}
+
+}
+}
